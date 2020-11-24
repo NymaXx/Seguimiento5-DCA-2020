8ba3f858b6bc9883aafe1330a5985f76bef1cb44
NymaXx <53951684+NymaXx@users.noreply.github.com>
2020-11-24 17:22:02 -0500

    Se pintan los infectados y se cuentan activamente los infectados y los sanos, sin embargo los infectados aparecen en lugares lejos de donde se infectaron y empiezan una ola de infeccion incorrecta...la posibilidad parece bien aplicada

--- src/Model/Logic.java
+++ src/Model/Logic.java
@@ -8,6 +8,9 @@ public class Logic implements Runnable {
 
 	private PApplet app;
 	private ArrayList<ModelPerson> persons;
+	private ArrayList<ModelHealthy> health;
+	private ArrayList<ModelInfected> infect;
+	private ArrayList<ModelRecovered> recov;
 	String[] data;
 	int counter;
 	int sanoCount, infecCount, recCount;
@@ -17,35 +20,38 @@ public class Logic implements Runnable {
 		counter=15;
 		
 		persons = new ArrayList<ModelPerson>();
+		health = new ArrayList <ModelHealthy>();
+		infect = new ArrayList <ModelInfected>();
+		recov = new ArrayList <ModelRecovered>();
 		
 		String[] data = app.loadStrings("data.txt");
 		String[] sanos = data[0].split(":",2);
 		int nSanos = Integer.parseInt(sanos[1]);
 		for (int i = 0; i < nSanos; i++) {
-			ModelPerson s = new ModelHealthy(app);
-			persons.add(s);
+			ModelHealthy h = new ModelHealthy(app);
+			health.add(h);
 			//OJO PACHO
-			sanoCount = nSanos;
-			new Thread(s).start();
+			sanoCount = health.size();
+			new Thread(h).start();
 		}
 		String[] infectados = data[1].split(":",2);
 		int nInfectados = Integer.parseInt(infectados[1]);
 		for (int i = 0; i < nInfectados; i++) {
-			ModelPerson s = new ModelInfected(app);
-			persons.add(s);
+			ModelInfected f = new ModelInfected(app);
+			infect.add(f);
 			//OJO PACHO X2
-			infecCount = nInfectados;
-			new Thread(s).start();
+			infecCount = infect.size();
+			new Thread(f).start();
 		}
 		String[] recuperados = data[2].split(":",2);
 		int nRecuperados = Integer.parseInt(recuperados[1]);
 		for (int i = 0; i < nRecuperados; i++) {
-			ModelPerson s = new ModelRecovered(app);
-			persons.add(s);
+			ModelRecovered r = new ModelRecovered(app);
+			recov.add(r);
 			//OJO PACHO X3
-			recCount = nRecuperados;
+			recCount = recov.size();
 			
-			new Thread(s).start();
+			new Thread(r).start();
 		
 		}
 		
@@ -53,8 +59,14 @@ public class Logic implements Runnable {
 
 	public void pintar() {
 		
-		for (int i = 0; i < persons.size(); i++) {
-			persons.get(i).pintar();
+		for (int i = 0; i < health.size(); i++) {
+			health.get(i).pintar();
+		}
+		for (int i = 0; i < infect.size(); i++) {
+			infect.get(i).pintar();
+		}
+		for (int i = 0; i < recov.size(); i++) {
+			recov.get(i).pintar();
 		}
 		app.noStroke();
 		app.rect(0,0,700,70);
@@ -63,6 +75,7 @@ public class Logic implements Runnable {
 		app.text("Sanos    " + sanoCount, 20,20);
 		app.text("Infectados    " + infecCount, 20,35);
 		app.text("Recuperados    " + recCount, 20,50);
+		
 		
 	}
 	
@@ -92,61 +105,43 @@ public class Logic implements Runnable {
 		}
 			}
 		}*/
-		System.out.println(counter);
+		
 		
 	}
 	public void run() {
 	
 		while(true) {
-			try {
-				for (int i = 0; i < persons.size(); i++) {
-					for (int j = 0; j < persons.size(); j++) {
-						ModelPerson a = persons.get(i);
-						ModelPerson b = persons.get(j);
+				for (int i = 0; i < infect.size(); i++) {
+					for (int j = 0; j < health.size(); j++) {
+						ModelInfected a = infect.get(i);
+						ModelHealthy b = health.get(j);
 						if (PApplet.dist(a.getPosX(), a.getPosY(), b.getPosX(), b.getPosY())<=a.getRadio()*2) {
 							a.setSpeedX(a.getSpeedX()*-1);
 							a.setSpeedY(a.getSpeedY()*-1);
 							b.setSpeedX(b.getSpeedX()*-1);
 							b.setSpeedY(b.getSpeedY()*-1);
-							if (a instanceof ModelInfected && b instanceof ModelHealthy) {
+							/*if (a instanceof ModelInfected && b instanceof ModelHealthy) {*/
 								float posibility = app.random(0,1);
 								int x = b.posX;
-								int y = b.posX;
+								int y = b.posY;
 								if(posibility > 0.1) {
 								b.setVivo(false);
-								persons.remove(b);
-								ModelPerson c = new ModelInfected(app);
-								c.setPosX(x);
-								c.setPosY(y);
-								persons.add(c);
-								
-								//sanoCount = persons.size();
-								
-								new Thread(c).start();}
+								health.remove(b);
+								ModelInfected c = new ModelInfected(app);
+								a.setPosX(b.getPosX());
+								a.setPosY(b.getPosY());
+								infect.add(c);
+								sanoCount = health.size();
+								infecCount = infect.size();
+								new Thread(c).start();
 								break;
-							} else if (b instanceof ModelInfected && a instanceof ModelHealthy) {
-								float posibility = app.random(0,1);
-								int x = a.posX;
-								int y = a.posX;
-								if(posibility > 0.1) {
-								a.setVivo(false);
-								persons.remove(a);
-								ModelPerson c = new ModelInfected(app);
-								c.setPosX(x);
-								c.setPosY(y);
-								persons.add(c);
-								new Thread(c).start();}
-								break;
-							} 
-						}
-						
-						
-					
+							}  
+						//}	
 					}
 				}
 				
-				for (int i = 0; i < persons.size(); i++) {
-					ModelPerson a = persons.get(i);
+				for (int q = 0; q < persons.size(); q++) {
+					ModelPerson a = persons.get(q);
 					if(counter == 0 && a instanceof ModelInfected) {
 					if (a.isVivo() == false) {
 						int x = a.getPosX();
@@ -157,15 +152,17 @@ public class Logic implements Runnable {
 						b.setPosY(y);
 						persons.add(b);
 						new Thread(b).start();
+								}
+							}
+						}
 					}
 				}
-				}
-				
-				Thread.sleep(20);
-			} catch (PercentException message) {
-				// TODO Auto-generated catch block
-				//app.text("Mas del 30% ha sido infectado", app.width/2,20);
-			}
+	
+	}
+	public void vailadeExcep() throws PercentException{
+		if(sanoCount <= 30) {
+			throw new PercentException("Mas del 30% ha sido infectado");
+		
 		}
 	}
 	
